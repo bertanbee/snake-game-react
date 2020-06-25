38dc60e06eed84b88a25fa360a22672ea8ce1020
bertanbee <bertan1707@gmail.com>
2020-06-25 19:42:59 -0300

    Desinstalling package react-keypress

--- game/src/Game.js
+++ game/src/Game.js
@@ -2,80 +2,76 @@ import React from 'react';
 import './styles.css';
 
 function Game() {
-
-  
-
   return (
-    <div className="BOX" >
+    <div className="BOX">
     <div className="Game">
       <div className="block">
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
+        <div className="little-block" id="11"></div>
+        <div className="little-block" id="12"></div>
+        <div className="little-block" id="13"></div>
+        <div className="little-block" id="14"></div>
+        <div className="little-block" id="15"></div>
+        <div className="little-block" id="16"></div>
+        <div className="little-block" id="17"></div>
+        <div className="little-block" id="18"></div>
+        <div className="little-block" id="19"></div>
+        <div className="little-block" id="110"></div>
+        <div className="little-block" id="111"></div>
+        <div className="little-block" id="112"></div>
+        <div className="little-block" id="113"></div>
+        <div className="little-block" id="114"></div>
+        <div className="little-block" id="115"></div>
+        <div className="little-block" id="116"></div>
+        <div className="little-block" id="117"></div>
+        <div className="little-block" id="118"></div>
+        <div className="little-block" id="119"></div>
+        <div className="little-block" id="120"></div>
       </div>
 
       <div className="block">
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-
+       <div className="little-block" id="21"></div>
+        <div className="little-block" id="22"></div>
+        <div className="little-block" id="23"></div>
+        <div className="little-block" id="24"></div>
+        <div className="little-block" id="25"></div>
+        <div className="little-block" id="26"></div>
+        <div className="little-block" id="27"></div>
+        <div className="little-block" id="28"></div>
+        <div className="little-block" id="29"></div>
+        <div className="little-block" id="210"></div>
+        <div className="little-block" id="211"></div>
+        <div className="little-block" id="212"></div>
+        <div className="little-block" id="213"></div>
+        <div className="little-block" id="214"></div>
+        <div className="little-block" id="215"></div>
+        <div className="little-block" id="216"></div>
+        <div className="little-block" id="217"></div>
+        <div className="little-block" id="218"></div>
+        <div className="little-block" id="219"></div>
+        <div className="little-block" id="220"></div>
       </div>
 
       <div className="block">
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
-        <div className="little-block"></div>
+        <div className="little-block" id="11"></div>
+        <div className="little-block" id="12"></div>
+        <div className="little-block" id="13"></div>
+        <div className="little-block" id="14"></div>
+        <div className="little-block" id="15"></div>
+        <div className="little-block" id="16"></div>
+        <div className="little-block" id="17"></div>
+        <div className="little-block" id="18"></div>
+        <div className="little-block" id="19"></div>
+        <div className="little-block" id="110"></div>
+        <div className="little-block" id="111"></div>
+        <div className="little-block" id="112"></div>
+        <div className="little-block" id="113"></div>
+        <div className="little-block" id="114"></div>
+        <div className="little-block" id="115"></div>
+        <div className="little-block" id="116"></div>
+        <div className="little-block" id="117"></div>
+        <div className="little-block" id="118"></div>
+        <div className="little-block" id="119"></div>
+        <div className="little-block" id="120"></div>
 
       </div>
 
