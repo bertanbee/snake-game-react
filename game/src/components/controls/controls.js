936bd0ace7beb9b137cc61cbf4f915c38b4350c3
bertanbee <bertan1707@gmail.com>
2020-06-27 17:29:13 -0300

    Controls styled :100:

--- game/src/components/controls/controls.js
+++ game/src/components/controls/controls.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { FaArrowLeft, FaArrowRight, FaArrowDown, FaArrowUp } from 'react-icons/fa';
 
+import './styles.css';
+
 function Controls() {
     return (
         <div className="controls">
-            <FaArrowLeft></FaArrowLeft>
-            <FaArrowUp></FaArrowUp>
-            <FaArrowDown></FaArrowDown>
-            <FaArrowRight></FaArrowRight>
+            <FaArrowLeft className="icon"></FaArrowLeft>
+            <FaArrowUp className="icon"></FaArrowUp>
+            <FaArrowDown className="icon"></FaArrowDown>
+            <FaArrowRight className="icon"></FaArrowRight>
         </div>
     );
 }
