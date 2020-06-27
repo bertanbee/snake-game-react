fb03dcd58aaaf650c6f4b08d5d74eb72ba171c8e
bertanbee <bertan1707@gmail.com>
2020-06-27 17:01:59 -0300

    Control Arrows Added

--- game/src/Game.js
+++ game/src/Game.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './styles.css';
+import { FaArrowLeft, FaArrowRight, FaArrowDown, FaArrowUp } from 'react-icons/fa'
 
 import Box from './components/box';
 
 function Game() {
   return (
+    <>
     <Box></Box>
+    <div className="controls">
+      <FaArrowLeft></FaArrowLeft>
+      <FaArrowUp></FaArrowUp>
+      <FaArrowDown></FaArrowDown>
+      <FaArrowRight></FaArrowRight>
+    </div>
+    </>
   );
 }
 
